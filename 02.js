b19aca47c4c8e372738833d4c129456c4151c2ad
PedroRuar <pedrovinicius.sd@gmail.com>
2021-09-09 01:06:01 -0300

    Atividade Presencial

---
+++ 02.js
@@ -0,0 +1,10 @@
+const alunos = 30
+for (let index =0; index <= alunos; index++) {
+    if (index === 0) {
+        console.log('número é zero');
+    }else if (index % 2 ===0){
+        console.log('o número é par ' + index);
+    }else {
+        console.log('o número é ímpar ' +index);
+    }
+}
